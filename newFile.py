686cb85d89cc73cacf7c703ed9104b9965623b97
nathananiel staley <zachary88d@gmail.com>
2022-01-31 12:42:18 -0500

    Added Python

---
+++ newFile.py
@@ -0,0 +1,9 @@
+country = ["USA", "Nigeria"]
+popGrowth = 0
+
+country = input("Choose a country: ")
+if(country == "USA"):
+    popGrowth = 3.65
+if(country == "Nigeria"):
+    popGrowth = 5.34
+    
