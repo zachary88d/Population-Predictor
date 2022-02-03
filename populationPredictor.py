619f4e566c573572129c651e9e41b4cdeeb4f300
nathananiel staley <zachary88d@gmail.com>
2022-02-02 23:59:15 -0500

    Load Population

--- populationPredictor.py
+++ populationPredictor.py
@@ -11,8 +11,10 @@ df = country_df[country_df['name'].str.contains(country)]
 print(df)
 countryPop = int(df['pop2021'] * 1000)
 countryGrowth = df['GrowthRate']
-print('The population of %s is %f' %(country,countryPop))
-print('The growth rate of %s is %f' %(country,countryGrowth))
+print('The population of %s is %.0f' %(country,countryPop))
+print('The growth rate of %s is %.4f' %(country,countryGrowth))
 
 year = input("Of which year do you want to find the predicted population of %s? " %(country))
-predictedPop = countryPop*math.exp(countryGrowth*(year - 2021))
+time = int(year) - 2021
+predictedPop = countryPop*math.exp(countryGrowth*time)
+print('The predicted population of %s is expected to be %.0f' %(country,predictedPop))
