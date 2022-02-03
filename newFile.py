7c27c47c5fe41005e6ebba487b23dfd5035e61a0
nathananiel staley <zachary88d@gmail.com>
2022-02-02 23:39:46 -0500

    Load Population

--- newFile.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-#import matplotlib.pyplot as plt
-
-country_df = pd.read_csv('csvData.csv')
-print(country_df.head())
--- populationPredictor.py
+++ populationPredictor.py
@@ -1,8 +1,18 @@
+import math
 import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
 
-
-
 country_df = pd.read_csv('csvData.csv')
-print(country_df.head())
+
+country = input("Please select a country: ")
+
+df = country_df[country_df['name'].str.contains(country)]
+print(df)
+countryPop = int(df['pop2021'] * 1000)
+countryGrowth = df['GrowthRate']
+print('The population of %s is %f' %(country,countryPop))
+print('The growth rate of %s is %f' %(country,countryGrowth))
+
+year = input("Of which year do you want to find the predicted population of %s? " %(country))
+predictedPop = countryPop*math.exp(countryGrowth*(year - 2021))
